f6f0d9f164f73249c4eb3e2735910396844baa4f
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-30 17:26:55 -0500

    Adds POST request fo new user

--- api/server.js
+++ api/server.js
@@ -4,21 +4,21 @@ const server = express()
 server.use(express.json())
 
 // [POST] /api/users/:id (C of CRUD, create new user from JSON payload)
-server.post('/api/users/:id', (req, res) => {
+server.post('/api/users/', (req, res) => {
   // res.json('create user')
-  // const newUser = req.body
-  // Users.create(newUser)
-  //   .then(user => {
-  //     if (user) {
-  //       res.status(201).json(user)
-  //     } else {
-  //       res.status(400).json({ message: "Please provide name and bio for the user" })
-  //     }
-  //   })
-  //   .catch(err => {
-  //     console.log(err)
-  //     res.status(500).json({ message: "There was an error while saving the user to the database" })
-  //   })
+  const newUser = req.body
+  if (!newUser.name || !newUser.bio) {
+    res.status(400).json({ message: "Please provide name and bio for the user" })
+  } else {
+    Users.insert(newUser)
+      .then(user => {
+        res.status(201).json(user)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+      })
+  }
 })
 
 // [GET] /api/users/ (R of CRUD, fetch all users)
@@ -70,6 +70,19 @@ server.delete('/api/users/:id', (req, res) => {
 // [PUT] /api/users/:id (U of CRUD, update user by :id)
 server.put('/api/users/:id', (req, res) => {
   // res.json('update user')
+  const { id } = req.params
+  const changes = req.body
+  Users.update(id, changes)
+    .then(result => {
+      if (result) {
+        res.status(200).json(result)
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "The user information could not be modified" })
+    })
 })
 
 module.exports = server
