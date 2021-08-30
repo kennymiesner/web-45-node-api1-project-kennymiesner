15bcf40f50b1fc63a0e9b80ed6c0eb87b9386903
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-30 16:31:01 -0500

    Adds GET request for all users

--- api/server.js
+++ api/server.js
@@ -5,27 +5,48 @@ server.use(express.json())
 
 // [POST] /api/users/:id (C of CRUD, create new user from JSON payload)
 server.post('/api/users/:id', (req, res) => {
-  res.json('create user')
-})
-
-// [GET] /api/users/:id (R of CRUD, fetch user by :id)
-server.get('/api/users/:id', (req, res) => {
-  res.json('fetch user by id')
+  // res.json('create user')
+  // const newUser = req.body
+  // Users.create(newUser)
+  //   .then(user => {
+  //     if (user) {
+  //       res.status(201).json(user)
+  //     } else {
+  //       res.status(400).json({ message: "Please provide name and bio for the user" })
+  //     }
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //     res.status(500).json({ message: "There was an error while saving the user to the database" })
+  //   })
 })
 
 // [GET] /api/users/ (R of CRUD, fetch all users)
 server.get('/api/users/', (req, res) => {
-  res.json('fetch all users')
+  // res.json('fetch all users')
+  Users.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: 'The users information could not be retrieved' })
+    })
+})
+
+// [GET] /api/users/:id (R of CRUD, fetch user by :id)
+server.get('/api/users/:id', (req, res) => {
+  // res.json('fetch user by id')
 })
 
 // [DELETE] /api/users/:id (D of CRUD, delete user)
 server.delete('/api/users/:id', (req, res) => {
-  res.json('delete user')
+  // res.json('delete user')
 })
 
 // [PUT] /api/users/:id (U of CRUD, update user by :id)
 server.put('/api/users/:id', (req, res) => {
-  res.json('update user')
+  // res.json('update user')
 })
 
 module.exports = server
