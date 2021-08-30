3f7c29957b8c3d3175de4884b125e4a9c53ff108
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-30 16:11:36 -0500

    Adds beginning server skeleton structure

--- api/server.js
+++ api/server.js
@@ -3,4 +3,29 @@ const Users = require('./users/model')
 const server = express()
 server.use(express.json())
 
+// [POST] /api/users/:id (C of CRUD, create new user from JSON payload)
+server.post('/api/users/:id', (req, res) => {
+  res.json('create user')
+})
+
+// [GET] /api/users/:id (R of CRUD, fetch user by :id)
+server.get('/api/users/:id', (req, res) => {
+  res.json('fetch user by id')
+})
+
+// [GET] /api/users/ (R of CRUD, fetch all users)
+server.get('/api/users/', (req, res) => {
+  res.json('fetch all users')
+})
+
+// [DELETE] /api/users/:id (D of CRUD, delete user)
+server.delete('/api/users/:id', (req, res) => {
+  res.json('delete user')
+})
+
+// [PUT] /api/users/:id (U of CRUD, update user by :id)
+server.put('/api/users/:id', (req, res) => {
+  res.json('update user')
+})
+
 module.exports = server
