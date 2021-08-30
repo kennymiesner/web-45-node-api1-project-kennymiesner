863c6df282cbfa7f39d09975f1a882e52796e259
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-30 16:49:36 -0500

    Adds DELETE request for user by :id

--- api/server.js
+++ api/server.js
@@ -53,7 +53,18 @@ server.get('/api/users/:id', (req, res) => {
 
 // [DELETE] /api/users/:id (D of CRUD, delete user)
 server.delete('/api/users/:id', (req, res) => {
-  // res.json('delete user')
+  Users.remove(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user)
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "The user could not be removed" })
+    })
 })
 
 // [PUT] /api/users/:id (U of CRUD, update user by :id)
