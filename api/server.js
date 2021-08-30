ac9b1b6bc93e5a2fbf80b8f7d03292865527c671
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-30 16:34:09 -0500

    Adds GET request for user by :id

--- api/server.js
+++ api/server.js
@@ -37,6 +37,18 @@ server.get('/api/users/', (req, res) => {
 // [GET] /api/users/:id (R of CRUD, fetch user by :id)
 server.get('/api/users/:id', (req, res) => {
   // res.json('fetch user by id')
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user)
+      } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist' })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: err.message })
+    })
 })
 
 // [DELETE] /api/users/:id (D of CRUD, delete user)
